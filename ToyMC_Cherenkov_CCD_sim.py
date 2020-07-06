f0d39c8ea27a6b508d323780441b1cd00442dff1
julianjcuevas <julianjcuevas@gmail.com>
2020-07-06 08:55:04 -0500

    add isotropic source function and plot

--- ToyMC_Cherenkov_CCD_sim.py
+++ ToyMC_Cherenkov_CCD_sim.py
@@ -1,5 +1,33 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-def isotropic_source(E):
-    Vel_muon = E
+def isotropic_source():
+    '''
+    generates random isotropic azimuthal angle from uniform distribution [0,1]
+
+    '''
+    r = np.random.uniform(0,1)
+    phi = 2*np.pi*r
+
+    return phi
+
+
+def cosmic_ray_flux():
+
+#plot histogram to check for flat line along degrees
+
+N = 100000
+
+angle = []
+
+for n in range(N):
+    phi = isotropic_source()
+    angle.append(phi)
+
+n_bins = 40
+
+plt.hist(angle, bins = n_bins, color = '#377eb8', edgecolor = 'k')
+plt.xlabel("Angular Distribution")
+plt.ylabel("Counts")
+plt.title("Angular Distribution of Isotropic Source")
+plt.show()
